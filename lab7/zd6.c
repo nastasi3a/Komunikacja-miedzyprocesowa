5cba02a611682c057e1636c5bdc418743c99fb47
Anastasiia Hopchuk <anastasiiahopch@gmail.com>
2023-05-10 20:23:17 +0200

    Modyfikacja programu - rodzic bezczynnie oczekuje na zakończenie procesu potomnego za pomocą funkcji wait(NULL)

---
+++ lab7/zd6.c
@@ -0,0 +1,35 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    pid_t pid_potomka;
+    char* komunikat;
+    int powtorzen;
+    printf("Na razie działa jeden proces\n");
+    pid_potomka = fork();
+    switch (pid_potomka) {
+        case -1:
+            printf("Rozwidlenie procesu nie powiodło się\n");
+            exit(1);
+        case 0:
+            komunikat = "Jestem potomkiem";
+            powtorzen = 5;
+            break;
+        default:
+            komunikat = "Jestem rodzicem";
+            powtorzen = 10;
+            printf("Czekam na zakończenie procesu potomnego...\n");
+            wait(NULL);
+            printf("Proces potomny zakończył działanie\n");
+            break;
+    }
+    for (; powtorzen > 0; powtorzen--) {
+        puts(komunikat);
+        sleep(1);
+    }
+    printf("Koniec procesu %d\n", getpid());
+    return 0;
+}
